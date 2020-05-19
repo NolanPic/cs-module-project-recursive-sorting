4e503c50a92ae9e8b26e88c7d8d3016577fca8a5
Nolan Picini <nolanpic@gmail.com>
2020-05-19 16:00:17 -0700

    merge sort almost complete

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,9 +2,31 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-
-    # Your code here
-
+    
+    # the pointer for arrA
+    a_pointer = 0
+    # the pointer for arrB
+    b_pointer = 0
+    
+    for i in range(len(merged_arr)):
+        if arrA[a_pointer] < arrB[b_pointer] or b_pointer is -1:
+            # A is smaller and should be added next
+            merged_arr[i] = arrA[a_pointer]
+            # increment a's pointer only if it still
+            # has more elements
+            if a_pointer < len(arrA) -1:
+                a_pointer +=1
+            else:
+                a_pointer = -1
+        else:
+            # B is smaller and should be added next
+            merged_arr[i] = arrB[b_pointer]
+            # increment b's pointer only if it still
+            # has more elements
+            if b_pointer < len(arrB) - 1:
+                b_pointer +=1
+            else:
+                b_pointer = -1
 
     return merged_arr
 
@@ -12,10 +34,17 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
+    if len(arr) <= 1:
+        return arr
 
+    mid_index = len(arr) // 2
+    
+    left = arr[:mid_index]
+    right = arr[mid_index:]
+    
+    return merge(merge_sort(left), merge_sort(right))
 
-    return arr
-
+print(merge_sort([10, 2, 7, 5, 16, 3]))
 
 # implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
